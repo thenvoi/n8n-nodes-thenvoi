834912252ba26a6e8cdb1415ba3a1aa78da34256
Yaron D <yaron.drukh@thenvoi.com>
2025-10-25 18:44:27 +0300

    Added thoughts utils

---
+++ nodes/ThenvoiAgent/utils/thoughts/index.ts
@@ -0,0 +1,10 @@
+export {
+	extractModelThought,
+	augmentPromptForModelThoughts,
+	MODEL_THOUGHT_SYSTEM_PROMPT,
+} from './thoughtExtraction';
+export {
+	generateToolStartThought,
+	generateToolEndThought,
+	generateErrorThought,
+} from './syntheticThoughts';

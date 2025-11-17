a045d611139fbff6507e1f602e2ca7462c11a121
Yaron D <yaron.drukh@thenvoi.com>
2025-11-17 21:36:39 +0200

    Added agent prompt template

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const path = require('path');
-const { task, src, dest } = require('gulp');
+const { task, src, dest, series } = require('gulp');
 
 task('build:icons', copyIcons);
+task('build:docs', copyDocs);
+task('build', series('build:icons', 'build:docs'));
 
 function copyIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
@@ -13,4 +15,11 @@ function copyIcons() {
 	const credDestination = path.resolve('dist', 'credentials');
 
 	return src(credSource).pipe(dest(credDestination));
+}
+
+function copyDocs() {
+	const docsSource = path.resolve('docs', '**', '*');
+	const docsDestination = path.resolve('dist', 'docs');
+
+	return src(docsSource).pipe(dest(docsDestination));
 }
